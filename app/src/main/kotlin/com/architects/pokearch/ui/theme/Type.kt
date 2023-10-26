f61fb9021ecca8940181daffd47d4a50d2450b7b
Mj-br <ro7josemanuel@gmail.com>
2023-10-26 18:19:11 +0200

    Add Theme.kt, Color.kt and Type.kt

--- app/src/main/kotlin/com/architects/pokearch/ui/theme/Type.kt
+++ app/src/main/kotlin/com/architects/pokearch/ui/theme/Type.kt
@@ -2,18 +2,30 @@ package com.architects.pokearch.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.architects.pokearch.R
+
+val Roboto = FontFamily(
+    Font(R.font.roboto_light, FontWeight.Light),
+    Font(R.font.roboto_regular, FontWeight.Normal),
+    Font(R.font.roboto_medium, FontWeight.Medium),
+    Font(R.font.roboto_bold, FontWeight.Bold),
+)
+
+val RobotoCondensed = FontFamily(
+    Font(R.font.roboto_condensed_light, FontWeight.Light),
+    Font(R.font.roboto_condensed_regular, FontWeight.Normal),
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = Roboto,
         fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
+        fontSize = 16.sp
     )
     /* Other default text styles to override
     titleLarge = TextStyle(
