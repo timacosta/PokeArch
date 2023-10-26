0822313c17d687d1a07014c1e7123cbf9897bf88
Mj-br <ro7josemanuel@gmail.com>
2023-10-26 18:27:59 +0200

    Add Theme.kt, Color.kt and Type.kt

--- app/src/main/kotlin/com/architects/pokearch/ui/theme/Color.kt
+++ app/src/main/kotlin/com/architects/pokearch/ui/theme/Color.kt
@@ -10,7 +10,7 @@ val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
-// Theme colors of the Pokedex app
+// Theme colors
 
 val LightBlue = Color(0xFFBAC7FF)
 val lightGrey = Color(0xFFAAAAAA)
