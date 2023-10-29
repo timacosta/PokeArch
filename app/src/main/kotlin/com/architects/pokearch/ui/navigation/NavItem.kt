5ed2cf40ceb9654a2470f306ac405b6fdcdcb70f
jujob <jujo.bonillo90@gmail.com>
2023-10-29 19:46:18 +0100

    Refactorizar para quitar el when

--- app/src/main/kotlin/com/architects/pokearch/ui/navigation/NavItem.kt
+++ app/src/main/kotlin/com/architects/pokearch/ui/navigation/NavItem.kt
@@ -1,9 +1,10 @@
 package com.architects.pokearch.ui.navigation
 
+import androidx.navigation.NavBackStackEntry
 import androidx.navigation.NavType
 import androidx.navigation.navArgument
 
-sealed class NavItem(val baseRoute: String, navArgs: List<NavArg> = emptyList()){
+sealed class NavItem(val baseRoute: String, navArgs: List<NavArg> = emptyList()) {
 
     val route = run {
         val args = navArgs.map { "{${it.key}}" }
@@ -13,21 +14,37 @@ sealed class NavItem(val baseRoute: String, navArgs: List<NavArg> = emptyList())
     }
 
     val args = navArgs.map {
-        navArgument(it.key){ type = it.navType }
+        navArgument(it.key) { type = it.navType }
     }
 
-    object Main: NavItem("mainScreen")
+    object Main : NavItem("mainScreen")
 
-    object Home: NavItem("homeScreen")
+    object Home : NavItem("homeScreen")
 
-    object Team: NavItem("teamScreen")
+    object Team : NavItem("teamScreen")
 
-    object Detail: NavItem("detailScreen", listOf(NavArg.PokemonId)){
-        fun createRoute(pokemonId: Int):String = "$baseRoute/$pokemonId"
+    object Detail : NavItem("detailScreen", listOf(NavArg.PokemonId)) {
+        fun createRoute(pokemonId: Int): String = "$baseRoute/$pokemonId"
     }
 
 }
 
-enum class NavArg(val key: String, val navType: NavType<*>){
-    PokemonId("pokemonId", NavType.IntType)
+sealed class NavArg(val key: String, val navTypeWrapper: NavTypeWrapper) {
+
+    val navType = navTypeWrapper.navType
+
+    inline fun <reified T> getArg(navBackStackEntry: NavBackStackEntry): T =
+        navTypeWrapper.getArg(navBackStackEntry, key) as T
+
+    object PokemonId : NavArg("pokemonId", NavTypeWrapper.IntType)
+}
+
+sealed class NavTypeWrapper(val navType: NavType<*>) {
+    abstract fun getArg(navBackStackEntry: NavBackStackEntry, key: String): Any
+
+    object IntType : NavTypeWrapper(NavType.IntType) {
+        override fun getArg(navBackStackEntry: NavBackStackEntry, key: String): Any =
+            navBackStackEntry.arguments?.getInt(key) as Any
+    }
+
 }
--- app/src/main/kotlin/com/architects/pokearch/ui/navigation/Navigation.kt
+++ app/src/main/kotlin/com/architects/pokearch/ui/navigation/Navigation.kt
@@ -3,7 +3,6 @@ package com.architects.pokearch.ui.navigation
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavBackStackEntry
 import androidx.navigation.NavGraphBuilder
-import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -24,7 +23,7 @@ fun Navigation(){
             }
         }
         composable(NavItem.Detail){ backStackEntry ->
-            DetailScreen(backStackEntry.findArg(NavArg.PokemonId))
+            DetailScreen(NavArg.PokemonId.getArg(backStackEntry))
         }
     }
 
@@ -40,13 +39,4 @@ fun NavGraphBuilder.composable(
     ){
         content(it)
     }
-}
-
-private inline fun <reified T> NavBackStackEntry.findArg(navArg: NavArg): T {
-    val value = when(navArg.navType){
-        NavType.IntType -> arguments?.getInt(navArg.key)
-        else -> null
-    }
-    requireNotNull(value)
-    return value as T
 }
