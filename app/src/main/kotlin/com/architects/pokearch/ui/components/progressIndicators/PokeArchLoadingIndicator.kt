cbbf0e2712cea83e4edffcd36e95062305fbb377
Mj-br <ro7josemanuel@gmail.com>
2023-11-13 16:09:31 +0100

    Improve Loading Screen

--- app/src/main/kotlin/com/architects/pokearch/ui/components/progressIndicators/PokeArchLoadingIndicator.kt
+++ app/src/main/kotlin/com/architects/pokearch/ui/components/progressIndicators/PokeArchLoadingIndicator.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
@@ -14,9 +15,15 @@ fun PokeArchLoadingIndicator(
     color: Color = MaterialTheme.colorScheme.surfaceVariant,
     trackColor: Color = MaterialTheme.colorScheme.secondary,
 ) {
-    CircularProgressIndicator(
-        modifier = modifier.width(64.dp),
-        color = color,
-        trackColor = trackColor,
-    )
+    Box(
+        modifier = modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center,
+    ) {
+
+        CircularProgressIndicator(
+            modifier = modifier.width(64.dp),
+            color = color,
+            trackColor = trackColor
+        )
+    }
 }
