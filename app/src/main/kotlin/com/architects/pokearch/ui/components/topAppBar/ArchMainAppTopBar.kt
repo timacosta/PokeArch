0fefecb2024c986e80655721ae3a4fc52fa9bea4
Mj-br <ro7josemanuel@gmail.com>
2023-11-14 12:29:17 +0100

    Add scroll behavior

--- app/src/main/kotlin/com/architects/pokearch/ui/components/topAppBar/ArchMainAppTopBar.kt
+++ app/src/main/kotlin/com/architects/pokearch/ui/components/topAppBar/ArchMainAppTopBar.kt
@@ -1,21 +1,27 @@
 package com.architects.pokearch.ui.components.topAppBar
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
 import com.architects.pokearch.ui.main.state.SearchWidgetState
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ArchMainAppTopBar(
     searchWidgetState: SearchWidgetState,
+    scrollBehavior: TopAppBarScrollBehavior,
     onCloseClicked: () -> Unit,
     onSearchTriggeredClicked: () -> Unit,
     onSearchClicked: (String) -> Unit
-){
+) {
     when (searchWidgetState) {
         SearchWidgetState.CLOSED -> {
             ArchTopAppBar(
-                onSearchClicked = onSearchTriggeredClicked
+                onSearchClicked = onSearchTriggeredClicked,
+                scrollBehavior = scrollBehavior
             )
         }
+
         SearchWidgetState.OPENED -> {
             ArchSearchBar(
                 onSearchClicked = onSearchClicked,
