7b47aa2e9921d0807ff6a8c6f1361439811a1f92
Mj-br <ro7josemanuel@gmail.com>
2023-11-29 17:19:36 +0100

    Delete Launch Effect in MainScreen

--- app/src/main/kotlin/com/architects/pokearch/ui/main/MainScreen.kt
+++ app/src/main/kotlin/com/architects/pokearch/ui/main/MainScreen.kt
@@ -9,7 +9,6 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
@@ -34,12 +33,6 @@ fun MainScreen(
     val state by mainViewModel.uiState.collectAsStateWithLifecycle()
 
     val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
-
-    LaunchedEffect(state.isSearchIconVisible){
-        if (state.isSearchIconVisible) {
-            mainViewModel.updateSearchTextState("")
-        }
-    }
 
     Scaffold(
         modifier = modifier
