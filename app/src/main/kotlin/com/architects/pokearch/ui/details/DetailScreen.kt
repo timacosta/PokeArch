44d3074521e21464a8512d8d0988d4e84f303ecd
Tim Acosta Ross <ross@auxmoney.com>
2023-12-07 10:50:51 +0000

    48 - added scrollable behaviour

--- app/src/main/kotlin/com/architects/pokearch/ui/details/DetailScreen.kt
+++ app/src/main/kotlin/com/architects/pokearch/ui/details/DetailScreen.kt
@@ -18,8 +18,10 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Add
 import androidx.compose.material.icons.outlined.Scale
@@ -119,7 +121,7 @@ private fun DetailSuccessScreen(
     }
 
     Column(
-        modifier = modifier,
+        modifier = modifier.verticalScroll(rememberScrollState()),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Top,
     ) {
