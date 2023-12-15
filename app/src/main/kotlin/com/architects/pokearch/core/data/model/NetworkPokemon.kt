384a2447b2282e85216606d329d94a618c626670
Tim Acosta Ross <ross@auxmoney.com>
2023-12-15 12:45:51 +0000

    48 - removed getImageURL from network pokemon

--- app/src/main/kotlin/com/architects/pokearch/core/data/model/NetworkPokemon.kt
+++ app/src/main/kotlin/com/architects/pokearch/core/data/model/NetworkPokemon.kt
@@ -9,8 +9,4 @@ data class NetworkPokemon(
     val url: String,
 ) {
     fun getIndex():Int = url.split("/".toRegex()).dropLast(1).last().toIntOrNull() ?: 0
-    /*fun getImageUrl(): String {
-        return "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/" +
-                "pokemon/other/official-artwork/${getIndex()}.png"
-    }*/
 }
