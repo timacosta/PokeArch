886b22cd0dacc140091cf310387dfe59dadf0ba7
Mj-br <ro7josemanuel@gmail.com>
2024-03-04 23:22:13 +0100

    Optimize imports

--- usecases/src/main/kotlin/com/architects/pokearch/usecases/GetAccelerometerValue.kt
+++ usecases/src/main/kotlin/com/architects/pokearch/usecases/GetAccelerometerValue.kt
@@ -1,10 +1,5 @@
 package com.architects.pokearch.usecases
 
-import arrow.core.Either
-import com.architects.pokearch.domain.model.Pokemon
-import com.architects.pokearch.domain.model.PokemonInfo
-import com.architects.pokearch.domain.model.error.Failure
-import com.architects.pokearch.domain.repository.PokeArchRepositoryContract
 import com.architects.pokearch.domain.repository.SensorRepositoryContract
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
